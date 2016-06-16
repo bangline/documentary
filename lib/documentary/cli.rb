80ee01ec50970b0ce8c841b897b85c8f4bb74b84
Dave Kennedy <david@bangline.co.uk>
2016-06-17 00:36:33 +0100

    Generate docs from the api

--- lib/documentary/cli.rb
+++ lib/documentary/cli.rb
@@ -4,13 +4,26 @@ require 'documentary'
 module Documentary
   class Cli
     def self.run(args, out=STDOUT)
+      file_glob = "./**/*.rb"
+      config = { project: "My Project" }
+
       OptionParser.new do |opts|
-        opts.on_tail("-v", "--version", "Print version number") do
+        opts.on("-v", "--version", "Print version number") do
           require "documentary/version"
           out << "Documentary #{Documentary::VERSION}\n"
           exit
         end
+
+        opts.on("-d", "--directory glob", "Set directory path") do |glob|
+          file_glob = file_glob = glob
+        end
+
+        opts.on("-p", "--project project", "Set directory path") do |project|
+        end
       end.parse!
+
+      files = Dir.glob(file_glob)
+      Documentary::Generator.new(files, config).generate
     end
   end
 end
